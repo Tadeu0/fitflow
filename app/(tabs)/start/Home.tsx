ee00cdb8b9c39207641c4770b9834cca69721ae6
FRANCISCO TADEU ESCORCIO PEREIRA <franciscotadeuuu@gmail.com>
2025-10-16 17:33:13 -0300

    Update Home.tsx

--- app/(tabs)/start/Home.tsx
+++ app/(tabs)/start/Home.tsx
@@ -1,24 +1,104 @@
+import React from 'react';
 import { Link } from "expo-router";
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import React, { useState } from 'react';
 import {
   Image,
-  KeyboardAvoidingView,
+  SafeAreaView,
+  ScrollView,
   StyleSheet,
   Text,
-  TextInput,
-  TouchableOpacity,
-  View,
+  View
 } from "react-native";
 export default function Home() {
 
   return (
-    <KeyboardAvoidingView style={{flex: 1}} behavior="padding">
+    <SafeAreaView style={{alignItems: 'center'}}>
+   <View style={style.Topo}><Text style={{fontSize: 34, fontWeight: "bold",color:"black"}}>Inicio</Text>
+   
+   </View>
+   <ScrollView style={style.scroll}>
+    
+    <View>
+    <Image
+   style={style.imagem}
+   source={{
+     uri: "https://snack-code-uploads.s3.us-west-1.amazonaws.com/~asset/9e25e2dba69e62b0e2563a6500e5fbad",
+                    }}
+   >
+   </Image>
+   <Link href={"/(tabs)/itens-calculo/IMC"} style={{backgroundColor: 'red', }}></Link>
+      </View>
 
-    </KeyboardAvoidingView>
-  );
+
+       
+      <Image
+       style={style.imagem}
+       source={{
+         uri: "https://snack-code-uploads.s3.us-west-1.amazonaws.com/~asset/2f12bae018a9c703fd7d55745808c01c",
+                    }}
+                  />
+
+      
+    <Text>oooooooooooooooooooo</Text>
+    <Text>oooooooooooooooooooo</Text>
+    <Text>oooooooooooooooooooo</Text>
+    <Text>oooooooooooooooooooo</Text>
+    <Text>oooooooooooooooooooo</Text>
+    <Text>oooooooooooooooooooo</Text>
+    <Text>oooooooooooooooooooo</Text>
+        <Text>oooooooooooooooooooo</Text>
+    <Text>oooooooooooooooooooo</Text>
+    <Text>oooooooooooooooooooo</Text>
+    <Text>oooooooooooooooooooo</Text>
+    <Text>oooooooooooooooooooo</Text>
+    <Text>oooooooooooooooooooo</Text>
+    <Text>oooooooooooooooooooo</Text>
+        <Text>oooooooooooooooooooo</Text>
+    <Text>oooooooooooooooooooo</Text>
+    <Text>oooooooooooooooooooo</Text>
+    <Text>oooooooooooooooooooo</Text>
+    <Text>oooooooooooooooooooo</Text>
+    <Text>oooooooooooooooooooo</Text>
+    <Text>oooooooooooooooooooo</Text>
+        <Text>oooooooooooooooooooo</Text>
+    <Text>oooooooooooooooooooo</Text>
+    <Text>oooooooooooooooooooo</Text>
+    <Text>oooooooooooooooooooo</Text>
+    <Text>oooooooooooooooooooo</Text>
+    <Text>oooooooooooooooooooo</Text>
+    <Text>oooooooooooooooooooo</Text>
+        <Text>oooooooooooooooooooo</Text>
+    <Text>oooooooooooooooooooo</Text>
+    <Text>oooooooooooooooooooo</Text>
+    <Text>oooooooooooooooooooo</Text>
+    <Text>oooooooooooooooooooo</Text>
+    <Text>oooooooooooooooooooo</Text>
+    <Text>oooooooooooooooooooo</Text>
+   </ScrollView>
+</SafeAreaView>
+
+  )
 }
 
 const style = StyleSheet.create({
-
+Topo:{
+  backgroundColor: 'white',
+  height: 80,
+  alignItems: 'center',
+  justifyContent: 'center',
+  elevation: 10,
+  width: '100%',
+},
+scroll:{
+  backgroundColor: '',
+  width:'95%',
+  height: '90%',
+  padding: 5,
+},
+imagem:{
+   width: '100%',
+   minHeight: '30%',
+   marginTop: '20%',
+   borderRadius: 10,
+   elevation: 10,
+ },
 });
